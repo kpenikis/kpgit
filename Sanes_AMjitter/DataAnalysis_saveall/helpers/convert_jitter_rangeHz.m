8e857fd3d2ea9803ce6c3e64abff85e33166fee0
kpenikis <kpenikis@gmail.com>
2017-04-05 18:19:58 -0400

    removed old DataAnalysis folder, that saved all response data

--- Sanes_AMjitter/DataAnalysis_saveall/helpers/convert_jitter_rangeHz.m
+++
@@ -1,8 +0,0 @@
-function rangeHz = convert_jitter_rangeHz(jitter_ID,center_rate_Hz)
-% rangeHz = convert_jitter_rangeHz(jitter_ID,center_rate_Hz)
-%   takes integers that were used to ID the jitter vectors, and converts
-%   them to the corresponding range of AM rates in each vector.
-
-rangeHz = 2.^((log2(center_rate_Hz))+jitter_ID./100) - 2.^((log2(center_rate_Hz))-jitter_ID./100);
-
-end
--- Sanes_AMjitter/DataAnalysis_saveall/helpers/load_rate_vector.m
+++
@@ -1,7 +0,0 @@
-
-function rV = load_rate_vector(blocks,subject)
-
-stimdir = fullfile('/Users/kpenikis/Documents/SanesLab/Data/AMJitter/RawData',subject,sprintf('Block-%i_Stim',blocks(1)));
-rV = load(fullfile(stimdir,stim(1).stimfn));
-
-
